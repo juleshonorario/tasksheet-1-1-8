87c2bc953068f63831dd156424b50efb6f12a2bd
juleshonorario <153901269+juleshonorario@users.noreply.github.com>
2024-01-10 10:35:53 +0000

    tasksheet-1-1-8

--- Parameters.java
+++ Parameters.java
@@ -1,1 +1,33 @@
+public class Parameters {
+    public static void main(String[] args) {
+        int[] numbers = {4, 5, 10};
+        calculateCumulativeSum(numbers);
+    }
 
+    public static void calculateCumulativeSum(int... nums) {
+        int totalSum = 0;
+        System.out.println("Results:");
+
+        for (int i = 0; i < nums.length; i++) {
+            int paramSum = 0;
+            for (int j = 1; j <= nums [i]; j++) {
+                paramSum += j;
+            }
+            totalSum += paramSum;
+            System.out.printf("Parameters %d: %d = (%s)%n" , i + 1, paramSum, getCumulativeExpression(nums[1]));
+        }
+        System.out.println("\nTotal Sum: " + totalSum);
+    }
+    
+    private static String getCumulativeExpression(int num) {
+        StringBuilder expression = new StringBuilder();
+
+        for (int i = 1; i <= num; i++) {
+            expression.append(i);
+            if (i < num) {
+                expression.append("+");
+            }
+        }
+        return expression.toString();
+    }
+}
